b64ea4f1a47fd6e395758bf64e6cec3803a15914
CsMuj <mujtaba.chowdhury@csedge.org>
2022-11-19 14:41:58 -0500

    Final commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from re import X
+from email import message
 from flask import Flask, render_template, request
 from gpiozero import LED
 from time import sleep
@@ -6,12 +6,8 @@ from time import sleep
 app = Flask(__name__)
 led = LED(17)
 
-conditon = X
-while conditon == On:
-    led.on()
-    sleep(0.5)
-    led.off()
-    sleep(2)
+blink = False
+
 
 @app.route('/')
 def index():
@@ -23,11 +19,30 @@ def about():
 
 @app.route('/message',  methods=['POST'])
 def light():
-    message = request.form['message']
+    global blink 
 
-    print(message)
+    try:
+        message = request.form['message']
+
+        if message == "on":
+            blink = False
+            led.on()
+        elif message == "off":
+            blink = False
+            led.off()
+        elif message == 'blink':
+            blink = True
+            while blink == True:
+                led.on()
+                sleep(1)
+                led.off()
+                sleep(1)
+    
+    except:
+        pass
+    
     return render_template('index.html')
-
+    
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
